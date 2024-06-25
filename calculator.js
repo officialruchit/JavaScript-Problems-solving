95d83e7f0fe678897ff282812b7124172aee34c0
officialruchit <92086127+officialruchit@users.noreply.github.com>
2024-06-25 14:34:29 +0530

    add Simple Calculator
    add Simple Calculator

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function addition(n1, n2) {
+  return n1 + n2;
+}
+
+function substraction(n1, n2) {
+  return n1 - n2;
+}
+
+function multiplication(n1, n2) {
+  return n1 * n2;
+}
+
+function division(n1, n2) {
+  return n1 / n2;
+}
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(`Addition of num1 and num2 is ${addition(num1, num2)}`);
+console.log(`Subtraction of num1 and num2 is ${substraction(num1, num2)}`);
+console.log(`Multiplication of num1 and num2 is ${multiplication(num1, num2)}`);
+console.log(`Division of num1 and num2 is ${division(num1, num2)}`);
