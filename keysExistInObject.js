a957b41a044723b3a00e92789555fc03364c9ae3
officialruchit <92086127+officialruchit@users.noreply.github.com>
2024-06-25 14:45:00 +0530

    check Key Exists in an Object
    check Key Exists in an Object

---
+++ keysExistInObject.js
@@ -0,0 +1,9 @@
+let person = {
+  name: "Ruchit",
+  age: 22,
+  profession: "Engineer",
+};
+
+let keys = Object.keys(person);
+console.log(keys.includes("name"));
+console.log(keys.includes("salary"));
