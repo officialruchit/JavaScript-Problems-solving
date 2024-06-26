601e21526d6f02df0fda308e38a34c1d44e732af
officialruchit <92086127+officialruchit@users.noreply.github.com>
2024-06-26 16:54:02 +0530

    Count the Number of Vowels in String
    Count the Number of Vowels in String

---
+++ countVowel.js
@@ -0,0 +1,20 @@
+function countOfVowels(str) {
+  str = str.toLowerCase();
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str.charAt(i) == "a" ||
+      str.charAt(i) == "e" ||
+      str.charAt(i) == "i" ||
+      str.charAt(i) == "o" ||
+      str.charAt(i) == "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let str = "aiovvttnOgs";
+let numberOfVowels = countOfVowels(str);
+console.log(numberOfVowels);
